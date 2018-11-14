3f9ea0cfb50320f38a244b463154d02dd7d92458
Alexandre René <alexandre.rene@freeneurons.ca>
2018-11-14 11:38:24 +0100

    Executing `shim.load()` now also loads exceptions from `graph` module

--- theano_shim/__init__.py
+++ theano_shim/__init__.py
@@ -1,3 +1,11 @@
 from .core import *
+from . import core
 from . import graph
 from . import symbolic
+
+def load_theano():
+    load(True)
+
+def load(load_theano=True, reraise=False):
+    core.load(load_theano, reraise)
+    graph.load_exceptions()
--- theano_shim/graph.py
+++ theano_shim/graph.py
@@ -7,8 +7,19 @@ import itertools
 from . import core
 from .config import config as cf
 
+# Custom exceptions
 class TooCostly(Exception):
     pass
+# Theano exceptions
+class _MissingInputError(Exception):
+    pass
+MissingInputError = _MissingInputError
+def load_exceptions():
+    if cf.use_theano:
+        import theano.gof as gof
+        MissingInputError = gof.MissingInputError
+    else:
+        MissingInputError = _MissingInputError
 
 ######################
 # Graph manipulation
@@ -35,13 +46,16 @@ def compile(inputs, outputs, *args, **kwargs):
         raise ValueError("`shim.graph.function()` is undefined for non-symbolic outputs")
     return core.theano.function(inputs, outputs, *args, **kwargs)
 
-def eval(expr, inputs=None, max_cost=10):
+def eval(expr, givens=None, max_cost=10, if_too_costly='raise', inputs=None):
     """
     Obtain a numerical value by evaluating an expression's graph.
 
     If expr is shared (or shimmed shared) variable, its `get_value()` method is
     called and the result returned.
-    Otherwise, the result of `expr.eval(inputs)` is  if Theano is not loaded, `expr` is simply returned
+    Otherwise, a function is compiled and evaluated. `givens` can be used
+    to replace symbolic and shared variables during compilation.
+
+    If it's not a symbolic expression at all, `expr` is simply returned.
 
     Parameters
     ----------
@@ -49,7 +63,7 @@ def eval(expr, inputs=None, max_cost=10):
         The theano expression we want to evaluate. If a slice, each component
         of the slice ('start', 'stop', 'step') is evaluated.
         TODO: iterable
-    inputs: dict (optional; default: {})
+    givens: dict (optional; default: {})
         Dictionary of inputs to be past to `eval`.
     max_cost: int (optional; default: 10)
         Maximum allowable 'cost' of the expression: if `expr` is estimated to
@@ -58,6 +72,12 @@ def eval(expr, inputs=None, max_cost=10):
         Cost is estimated by a proxy, specifically the number of ancestors to
         `expr` in the Theano graph. By default, only expressions with 10 ancestors
         or less are evaluated.
+    if_too_costly: 'ignore'  |  'raise'
+        What to do if an expression is too costly to compute.
+        'ignore': do nothing, return symbolic expression.
+        'raise' : raise a TooCostly exception.
+    inputs: Deprecated
+        Synonym for `givens`.
 
     Returns
     -------
@@ -74,11 +94,20 @@ def eval(expr, inputs=None, max_cost=10):
     # "Standard" code path
     cost = len(core.gettheano().gof.graph.ancestors([expr]))
     if max_cost is not None and cost > max_cost:
-        raise TooCostly("Expression has {} ancestors, which exceeds the limit of {}."
-                        .format(cost, max_cost))
-    if inputs is None:
-        inputs = {}
-    return expr.eval(inputs)
+        if if_too_costly == 'raise':
+            raise TooCostly("Expression has {} ancestors, which exceeds the "
+                            "limit of {}.".format(cost, max_cost))
+        else:
+            return expr
+    else:
+        if inputs is None: inputs = {}  # TODO: Remove `inputs`
+        if givens is None: givens = {}
+        givens = {**inputs, **givens}
+        for k, v in givens.items():
+            givens[k] = core.cast(v, k.dtype)
+        f = core.gettheano().function(
+            [], expr, givens=givens, on_unused_input='ignore')
+        return f()
 
 ######################
 # Graph inspection
@@ -116,11 +145,42 @@ def inputs(varlist, *args, **kwargs):
     """
     if ( not isinstance(varlist, collections.Iterable)
          or isinstance(varlist, str) ):
-        raise ValueError("theano_shim.is_computable requires a list as first argument.")
+        raise ValueError("theano_shim.graph.inputs requires a list as first argument.")
     if core.is_theano_object(varlist):
         return core._gettheano().gof.graph.inputs(varlist, *args, **kwargs)
     else:
         return []
 
+def variables(i, o=None):
+    """
+    Wrapper for theano.gof.graph.variables.
+    Returns an empty list for non symbolic variables
+    If given only one argument, returns all variables it depends on.
+    """
+    if o is None:
+        o = i
+        i = inputs(o)
+    if ( not isinstance(i, collections.Iterable)
+         or isinstance(i, str) ):
+        raise ValueError(
+            "Arguments to theano_shim.graph.variables must be lists.")
+    if core.is_theano_object(i, o):
+        return core._gettheano().gof.graph.variables(i, o)
+    else:
+        return []
+
 def symbolic_inputs(varlist, *args, **kwargs):
     return [v for v in inputs(varlist, *args, **kwargs) if core.is_theano_variable(v)]
+
+def is_same_graph(var1, var2, givens=None, debug=False):
+    """
+    Wraps theano.gof.graph.is_same_graph().
+    Returns True if `var1` and `var2` perform the same computation.
+    If either `var1` or `var2` is not a graph object, returns the result of
+    `var1 == var2`.
+    """
+    if not (core.is_theano_object(var1) and core.is_theano_object(var2)):
+        return var1 == var2
+    else:
+        return core._gettheano().gof.graph.is_same_graph(var1, var2,
+                                                         givens, debug)
